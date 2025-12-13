5dc3fe5102489aa5923ac97425da430daeea37d6
Gugino <gugino.joshua@gmail.com>
2025-12-12 22:59:34 -0500

    Started work on collection crud

--- pb/pb-collection.go
+++ pb/pb-collection.go
@@ -2,6 +2,40 @@ package pb
 
 import "fmt"
 
+type CollectionType int
+
+const (
+	BaseCollection CollectionType = iota
+	AuthCollection
+	ViewCollection
+)
+
+func (c CollectionType) String() string {
+	switch c {
+	case 0:
+		return "base"
+	case 1:
+		return "auth"
+	case 2:
+		return "view"
+	default:
+		return "base"
+	}
+}
+
+type CollectionOptions struct {
+	Name       string           `json:"name"`
+	Type       CollectionType   `json:"type"`
+	Fields     []map[string]any `json:"fields"`
+	System     bool             `json:"system"`
+	ListRule   string           `json:"listRule"`
+	ViewRule   string           `json:"viewRule"`
+	CreateRule string           `json:"createRule"`
+	UpdateRule string           `json:"updateRule"`
+	DeleteRule string           `json:"deleteRule"`
+	ViewQuery  string           `json:"viewQuery"`
+}
+
 type PBCollection struct {
 	BaseURL string
 }
@@ -33,6 +67,19 @@ func (collection *PBCollection) UpdateCollection(token string, desiredCollection
 }
 
 // ### VIEW COLLECTION ###
+
+func (collection *PBCollection) ScaffoldCollections(token string, desiredCollection string) {
+	apiUrl := fmt.Sprintf("%s/api/collections/meta/scaffolds", collection.BaseURL)
+	res, err := SendAuthenticatedHTTPRequest("GET", apiUrl, map[string]string{}, map[string]any{}, token)
+
+	if err != nil {
+		return
+	}
+
+	fmt.Println(res)
+
+}
+
 func (collection *PBCollection) ViewCollection(token string, desiredCollection string) {
 	apiUrl := fmt.Sprintf("%s/api/collections/%s", collection.BaseURL, desiredCollection)
 	res, err := SendAuthenticatedHTTPRequest("GET", apiUrl, map[string]string{}, map[string]any{}, token)
@@ -60,6 +107,17 @@ func (collection *PBCollection) ListCollections(token string) {
 // ### DELETE COLLECTION ###
 func (collection *PBCollection) DeleteCollection(token string, desiredCollection string) {
 	apiUrl := fmt.Sprintf("%s/api/collections/%s", collection.BaseURL, desiredCollection)
+	res, err := SendAuthenticatedHTTPRequest("DELETE", apiUrl, map[string]string{}, map[string]any{}, token)
+
+	if err != nil {
+		return
+	}
+
+	fmt.Println(res)
+}
+
+func (collection *PBCollection) TruncateCollection(token string, desiredCollection string) {
+	apiUrl := fmt.Sprintf("%s/api/collections/%s/truncate", collection.BaseURL, desiredCollection)
 	res, err := SendAuthenticatedHTTPRequest("DELETE", apiUrl, map[string]string{}, map[string]any{}, token)
 
 	if err != nil {
--- pb/pb-structs.go
+++ pb/pb-structs.go
@@ -1,47 +1,5 @@
 package pb
 
-type AuthMethodResponse struct {
-	Password PwdAuthMethod    `json:"password"`
-	OAuth2   OAuth2AuthMethod `json:"oauth2"`
-	MFA      MFAAuthMethod    `json:"mfa"`
-	OTP      OTPAuthMethod    `json:"otp"`
-}
-
-type PwdAuthMethod struct {
-	Enabled        bool     `json:"enabled"`
-	IdentityFields []string `json:"identityFields"`
-}
-
-type OAuth2AuthMethod struct {
-	Enabled   bool            `json:"enabled"`
-	Providers []OAuthProvider `json:"providers"`
-}
-
-type MFAAuthMethod struct {
-	Enabled  bool `json:"enabled"`
-	Duration int  `json:"duration"`
-}
-
-type OTPAuthMethod struct {
-	Enabled  bool `json:"enabled"`
-	Duration int  `json:"duration"`
-}
-
-type OAuthProvider struct {
-	Name                string `json:"name"`
-	DisplayName         string `json:"displayName"`
-	State               string `json:"state"`
-	AuthURL             string `json:"authURL"`
-	CodeVerifier        string `json:"codeVerifier"`
-	CodeChallenge       string `json:"codeChallenge"`
-	CodeChallengeMethod string `json:"codeChallengeMethod"`
-}
-
-type AuthSuccessResponse struct {
-	Token  string         `json:"token"`
-	Record map[string]any `json:"record"`
-}
-
 type PocketBaseErrorResponse struct {
 	Status  string         `json:"status"`
 	Message string         `json:"message"`
